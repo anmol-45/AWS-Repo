a31228128f328cf2c356ba7f9378eefe6b1c6db6
Anmol Kumar <anmol_kumar@epam.com>
2025-03-06 11:41:53 +0530

    task added

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,19 +1,19 @@
 exports.handler = async (event) => {
-    const path = event.rawPath || event.path;
-    const method = event.requestContext?.http?.method || event.httpMethod;
+    const httpMethod = event.httpMethod;
+    const path = event.path;
 
-    if (path === "/hello" && method === "GET") {
-        return {
-            statusCode: 200,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({statusCode : 200,  message: "Hello from Lambda" }),
-        };
+    const response = {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: 'Hello from Lambda!' })
+    };
+
+    if (path === '/hello' && httpMethod === 'GET') {
+        response.body = JSON.stringify({ message: 'Hello from Lambda!' });
+    } else {
+        response.statusCode = 400;
+        response.body = JSON.stringify({ error: 'Invalid request' });
     }
 
-    return {
-        statusCode: 400,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-        "statusCode": 400,
-        "message": `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-        }),
+    return response;
+};
